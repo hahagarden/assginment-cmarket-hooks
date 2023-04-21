8f9d35752e0fb011e9604a7fdbe24416afe2b260
hahagarden <tingyuan413@gmail.com>
2023-04-21 17:29:33 +0900

    feat: complete App, Nav, ItemListContainer, ShoppingCart

--- src/App.js
+++ src/App.js
@@ -1,31 +1,30 @@
-import React, { useState } from 'react';
-import Nav from './components/Nav';
-import ItemListContainer from './pages/ItemListContainer';
-import './App.css';
-import './variables.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import ShoppingCart from './pages/ShoppingCart';
-import { initialState } from './assets/state';
+import React, { useState } from "react";
+import Nav from "./components/Nav";
+import ItemListContainer from "./pages/ItemListContainer";
+import "./App.css";
+import "./variables.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ShoppingCart from "./pages/ShoppingCart";
+import { initialState } from "./assets/state";
 
 function App() {
   const [items, setItems] = useState(initialState.items);
   const [cartItems, setCartItems] = useState(initialState.cartItems);
 
+  const getQuantityOfCartItems = () => {
+    let total = 0;
+    cartItems.forEach((item) => (total += item.quantity));
+    return total;
+  };
+
   return (
     <Router>
-      <Nav />
+      <Nav total={getQuantityOfCartItems()} />
       <Routes>
-        <Route path="/" element={<ItemListContainer items={items} />} />
-        <Route
-          path="/shoppingcart"
-          element={<ShoppingCart cartItems={cartItems} items={items} />}
-        />
+        <Route path="/" element={<ItemListContainer items={items} cartItems={cartItems} setCartItems={setCartItems} />} />
+        <Route path="/shoppingcart" element={<ShoppingCart cartItems={cartItems} setCartItems={setCartItems} items={items} />} />
       </Routes>
-      <img
-        id="logo_foot"
-        src={`${process.env.PUBLIC_URL}/codestates-logo.png`}
-        alt="logo_foot"
-      />
+      <img id="logo_foot" src={`${process.env.PUBLIC_URL}/codestates-logo.png`} alt="logo_foot" />
     </Router>
   );
 }
--- src/pages/ItemListContainer.js
+++ src/pages/ItemListContainer.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import Item from '../components/Item';
+import React from "react";
+import Item from "../components/Item";
 
-function ItemListContainer({ items }) {
-  const handleClick = () => { }
+function ItemListContainer({ items, cartItems, setCartItems }) {
+  const handleClick = (event, id) => {
+    const newCartItems = cartItems.slice();
+    if (newCartItems.filter((item) => item.itemId === id).length === 0) newCartItems.push({ itemId: id, quantity: 1 });
+    else newCartItems.filter((item) => item.itemId === id)[0].quantity++;
+    setCartItems(newCartItems);
+  };
+
   return (
     <div id="item-list-container">
       <div id="item-list-body">
         <div id="item-list-title">쓸모없는 선물 모음</div>
-        {items.map((item, idx) => <Item item={item} key={idx} handleClick={handleClick} />)}
+        {items.map((item, idx) => (
+          <Item item={item} key={idx} handleClick={handleClick} />
+        ))}
       </div>
     </div>
   );
